e1fadda67c0786f88d64a810c88ef3ceb4edc17b
Joakim Anell <an3ll@Joakims-MBP.pricer.com>
2019-10-21 09:47:23 +0200

    bumped some dependency-versions

--- buildSrc/src/main/kotlin/se/an3ll/bestallning/build/Dependencies.kt
+++ buildSrc/src/main/kotlin/se/an3ll/bestallning/build/Dependencies.kt
@@ -2,15 +2,15 @@ package se.an3ll.bestallning.build
 
 object Dependencies {
 
-  const val logbackVersion= "1.2.1"
-  const val ktorVersion= "1.2.0"
-  const val kotlinVersion= "1.3.31"
-  const val koinVersion = "2.0.0-GA6"
+  const val logbackVersion = "1.2.1"
+  const val ktorVersion = "1.2.5"
+  const val kotlinVersion = "1.3.31"
+  const val koinVersion = "2.0.1"
   const val postgresVersion = "42.2.5"
-  const val exposedVersion = "0.13.7"
+  const val exposedVersion = "0.17.6"
   const val hikariVersion = "3.3.1"
   const val flywayVersion = "5.2.4"
-  const val spekVersion = "2.0.4"
+  const val spekVersion = "2.0.8"
   const val mockkVersion = "1.9.3"
 
 }
